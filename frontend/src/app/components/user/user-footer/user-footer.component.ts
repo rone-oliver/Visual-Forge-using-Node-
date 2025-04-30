b02662783467528427dd00bb02ecdfc49f099918
Antony Rone Oliver <roneootan611@gmail.com>
2025-04-30 11:10:18 +0530

    modified footer of the user side.

--- frontend/src/app/components/user/user-footer/user-footer.component.ts
+++ frontend/src/app/components/user/user-footer/user-footer.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, Instagram, Linkedin, Facebook, Twitter } from 'lucide-angular';
 
 @Component({
   selector: 'app-user-footer',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './user-footer.component.html',
   styleUrl: './user-footer.component.scss'
 })
 export class UserFooterComponent {
-
+  icons = {
+    instagram: Instagram,
+    twitter: Twitter,
+    linkedin: Linkedin,
+    facebook: Facebook,
+  }
 }
