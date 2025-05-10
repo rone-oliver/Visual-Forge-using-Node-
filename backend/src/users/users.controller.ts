0b73005ee16f68d2bf3dc535bad2c679e0848667
Antony Rone Oliver <roneootan611@gmail.com>
2025-05-10 15:10:36 +0530

    modified the routes on the user.service(frontend) and users controller to follow rest principles.

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Get, Logger, Param, Patch, Post, Query, Req, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Logger, Param, Patch, Post, Put, Query, Req, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
 import { Types } from 'mongoose';
 import { UsersService } from './users.service';
 import { FilesInterceptor } from '@nestjs/platform-express';
@@ -15,92 +15,79 @@ export class UsersController {
     constructor(
         private userService: UsersService,
         private editorService: EditorsService
-    ){};
+    ) { };
 
     @Get('profile')
-    @Roles('User','Editor')
-    async getUserProfile(@Req() req: Request){
+    @Roles('User', 'Editor')
+    async getUserProfile(@Req() req: Request) {
         console.log('controlled hitted on /user/profile');
-        const user = req['user'] as { userId: Types.ObjectId; role: string}
+        const user = req['user'] as { userId: Types.ObjectId; role: string }
         const userDet = await this.userService.getUserDetails(user.userId);
-        console.log('user profile data',userDet);
+        console.log('user profile data', userDet);
         return userDet;
     }
 
-    @Post('editorRequest')
+    @Post('editor-requests')
     @Roles('User')
-    async requestForEditor(@Req() req: Request){
-        const user = req['user'] as { userId: Types.ObjectId; role: string};
+    async requestForEditor(@Req() req: Request) {
+        const user = req['user'] as { userId: Types.ObjectId; role: string };
         const response = await this.userService.requestForEditor(user.userId);
         return response;
     }
 
-    @Get('editorRequest/status')
+    @Get('editor-requests')
     @Roles('User')
-    async getEditorRequestStatus(@Req() req: Request){
-        const user = req['user'] as { userId: Types.ObjectId; role: string};
+    async getEditorRequestStatus(@Req() req: Request) {
+        const user = req['user'] as { userId: Types.ObjectId; role: string };
         const status = await this.userService.getEditorRequestStatus(user.userId);
-        return {status};
+        return { status };
     }
 
     @Get('quotations')
-    @Roles('User','Editor')
-    async getQuotations(@Req() req: Request){
-        const user = req['user'] as { userId: Types.ObjectId; role: string};
+    @Roles('User', 'Editor')
+    async getQuotations(@Req() req: Request) {
+        const user = req['user'] as { userId: Types.ObjectId; role: string };
         const quotations = await this.userService.getQuotations(user.userId);
         return quotations;
     }
 
-    @Post('create-quotation')
-    @Roles('User','Editor')
-    async createQuotation(@Req() req: Request, @Body() body){
-        const user = req['user'] as { userId: Types.ObjectId; role: string};
-        const success = await this.userService.createQuotation(body.quotation,user.userId);
-        if(success){
+    @Post('quotations')
+    @Roles('User', 'Editor')
+    async createQuotation(@Req() req: Request, @Body() body) {
+        const user = req['user'] as { userId: Types.ObjectId; role: string };
+        const success = await this.userService.createQuotation(body.quotation, user.userId);
+        if (success) {
             return true;
         }
         return false;
     }
 
-    @Patch('profile-image')
-    @Roles('User','Editor')
-    async updateProfileImage(@Req() req: Request, @Body() body){
-        const user = req['user'] as { userId: Types.ObjectId, role: string}
+    @Patch('profile/image')
+    @Roles('User', 'Editor')
+    async updateProfileImage(@Req() req: Request, @Body() body) {
+        const user = req['user'] as { userId: Types.ObjectId, role: string }
         const success = await this.userService.updateProfileImage(body.url, user.userId);
-        if(success){
+        if (success) {
             return true;
         }
         return false;
     }
 
-    @Post('quotation/files-upload')
-    @Roles('User','Editor')
-    @UseInterceptors(FilesInterceptor('files',5))
-    async uploadFile(
-        @Req() req: Request, 
-        @UploadedFiles() files: Express.Multer.File[],
-        @Body('folder') folder?: string,
-    ){
-        this.logger.log(`Uploading ${files.length} files`);
-        const result = await this.userService.uploadFiles(files,folder);
-        return result;
-    }
-
-    @Patch('profile/update')
-    @Roles('User','Editor')
-    async updateProfile(@Req() req: Request, @Body() body){
-        const user = req['user'] as { userId: Types.ObjectId, role: string}
+    @Patch('profile')
+    @Roles('User', 'Editor')
+    async updateProfile(@Req() req: Request, @Body() body) {
+        const user = req['user'] as { userId: Types.ObjectId, role: string }
         const success = await this.userService.updateProfile(body, user.userId);
-        if(success){
+        if (success) {
             return true;
         }
         return false;
     }
 
     @Patch('reset-password')
-    @Roles('User','Editor')
-    async resetPassword(@Req() req: Request, @Body() body:{currentPassword: string, newPassword: string}){
-        const user = req['user'] as { userId: Types.ObjectId, role: string}
+    @Roles('User', 'Editor')
+    async resetPassword(@Req() req: Request, @Body() body: { currentPassword: string, newPassword: string }) {
+        const user = req['user'] as { userId: Types.ObjectId, role: string }
         try {
             const response = await this.userService.resetPassword(body, user.userId);
             return response;
@@ -109,44 +96,38 @@ export class UsersController {
         }
     }
 
+    @Post('quotations/upload')
+    @Roles('User', 'Editor')
+    @UseInterceptors(FilesInterceptor('files', 5))
+    async uploadFile(
+        @Req() req: Request,
+        @UploadedFiles() files: Express.Multer.File[],
+        @Body('folder') folder?: string,
+    ) {
+        this.logger.log(`Uploading ${files.length} files`);
+        const result = await this.userService.uploadFiles(files, folder);
+        return result;
+    }
+
     @Get('quotations/completed')
-    @Roles('User','Editor')
-    async getCompletedWorks(@Req() req: Request){
-        const user = req['user'] as { userId: Types.ObjectId, role: string}
+    // GET /quotations?status=completed
+    @Roles('User', 'Editor')
+    async getCompletedWorks(@Req() req: Request) {
+        const user = req['user'] as { userId: Types.ObjectId, role: string }
         const quotations = await this.userService.getCompletedWorks(user.userId);
         return quotations;
     }
 
-    @Post('quotations/rate')
-    @Roles('User','Editor')
-    async rateWork(@Req() req: Request, @Body() body:{workId: string, rating: number, feedback: string}){
-        const user = req['user'] as { userId: Types.ObjectId, role: string}
-        console.log('worksId from controller:',body.workId);
-        const success = await this.userService.rateWork(body.workId, body.rating, body.feedback);
-        if(success){
+    @Put('quotations/:workId/rating')
+    @Roles('User', 'Editor')
+    async rateWork(@Req() req: Request, @Param('workId') workId: string, @Body() body: { rating: number, feedback: string }) {
+        const user = req['user'] as { userId: Types.ObjectId, role: string }
+        console.log('worksId from controller:', workId);
+        const success = await this.userService.rateWork(workId, body.rating, body.feedback);
+        if (success) {
             return true;
         }
         return false;
-    }
-
-    @Post('editor/rate')
-    @Roles('User','Editor')
-    async rateEditor(@Req() req: Request, @Body() body:{editorId: string, rating: number, feedback: string}){
-        this.logger.log('rating editor:',body.editorId,body.rating,body.feedback);
-        const user = req['user'] as { userId: Types.ObjectId, role: string}
-        const success = await this.userService.rateEditor(body.editorId, body.rating, body.feedback, user.userId);
-        if(success){
-            return true;
-        }
-        return false;
-    }
-
-    @Get('editor/rating')
-    @Roles('User','Editor')
-    async getCurrentEditorRating(@Req() req: Request, @Query('editorId') editorId: string){
-        const user = req['user'] as { userId: Types.ObjectId, role: string}
-        const rating = await this.userService.getCurrentEditorRating(editorId,user.userId);
-        return rating;
     }
 
     @Patch('quotations/:workId/public')
@@ -158,10 +139,30 @@ export class UsersController {
         return false
     }
 
+    @Post('editor/rating')
+    @Roles('User', 'Editor')
+    async rateEditor(@Req() req: Request, @Body() body: { editorId: string, rating: number, feedback: string }) {
+        this.logger.log('rating editor:', body.editorId, body.rating, body.feedback);
+        const user = req['user'] as { userId: Types.ObjectId, role: string }
+        const success = await this.userService.rateEditor(body.editorId, body.rating, body.feedback, user.userId);
+        if (success) {
+            return true;
+        }
+        return false;
+    }
+
+    @Get('editor/rating')
+    @Roles('User', 'Editor')
+    async getCurrentEditorRating(@Req() req: Request, @Query('editorId') editorId: string) {
+        const user = req['user'] as { userId: Types.ObjectId, role: string }
+        const rating = await this.userService.getCurrentEditorRating(editorId, user.userId);
+        return rating;
+    }
+
     @Get('works/public')
-    @Roles('User','Editor')
-    async getPublicWorks(@Req() req: Request, @Query('page') page: number, @Query('limit') limit: number){
-        const user = req['user'] as { userId: Types.ObjectId, role: string}
+    @Roles('User', 'Editor')
+    async getPublicWorks(@Req() req: Request, @Query('page') page: number, @Query('limit') limit: number) {
+        const user = req['user'] as { userId: Types.ObjectId, role: string }
         const works = await this.userService.getPublicWorks(page, limit);
         return works;
     }
@@ -171,7 +172,7 @@ export class UsersController {
         return this.userService.getUser(id);
     }
 
-    @Get('getEditor/:id')
+    @Get('editors/:id')
     async getEditor(@Param('id') id: string): Promise<User & { editorDetails?: any } | null> {
         return this.editorService.getEditor(id);
     }
--- frontend/src/app/services/user/user.service.ts
+++ frontend/src/app/services/user/user.service.ts
@@ -11,78 +11,79 @@ import { CompletedWork, Works } from '../../interfaces/completed-word.interface'
 })
 export class UserService {
   private apiUrl = environment.apiUrl;
+  private userApiUrl = this.apiUrl + '/user';
 
   constructor(
     private http: HttpClient,
   ) { };
 
-  getUserProfile(){
-    return this.http.get<User>(`${this.apiUrl}/user/profile`).pipe(
-      map((response:any)=> {
-        console.log(`user profile response: `,response);
+  getUserProfile() {
+    return this.http.get<User>(`${this.userApiUrl}/profile`).pipe(
+      map((response: any) => {
+        console.log(`user profile response: `, response);
         return response;
       }),
-      catchError((error)=>{
+      catchError((error) => {
         console.error('Error fetching user profile:', error);
         throw error;
       })
     );
   }
 
-  requestForEditor(){
-    return this.http.post<boolean>(`${this.apiUrl}/user/editorRequest`,{},{withCredentials: true});
+  requestForEditor() {
+    return this.http.post<boolean>(`${this.userApiUrl}/editor-requests`, {});
   }
 
-  getEditorRequestStatus(): Observable<string | null>{
-    return this.http.get<{status: string}>(`${this.apiUrl}/user/editorRequest/status`,{withCredentials: true}).pipe(
-      map(response=>response.status),
-      catchError(()=> of(null))
+  getEditorRequestStatus(): Observable<string | null> {
+    return this.http.get<{ status: string }>(`${this.userApiUrl}/editor-requests`).pipe(
+      map(response => response.status),
+      catchError(() => of(null))
     );
   }
 
-  getQuotations(): Observable<any[]>{
-    return this.http.get<any[]>(`${this.apiUrl}/user/quotations`).pipe(
-      map((response)=>response),
-      catchError((error)=> {throw error})
+  getQuotations(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.userApiUrl}/quotations`).pipe(
+      map((response) => response),
+      catchError((error) => { throw error })
     )
   }
 
-  createQuotation(quotation:any): Observable<boolean>{
-    return this.http.post<boolean>(`${this.apiUrl}/user/create-quotation`,{quotation}).pipe(
-      map(response=>response),
-      catchError(err=>{ throw err})
+  createQuotation(quotation: any): Observable<boolean> {
+    return this.http.post<boolean>(`${this.userApiUrl}/quotations`, { quotation }).pipe(
+      map(response => response),
+      catchError(err => { throw err })
     )
   }
 
-  updateProfileImage(url: string): Observable<boolean>{
-    return this.http.patch<boolean>(`${this.apiUrl}/user/profile-image`,{ url}).pipe(
-      map(response=>response),
-      catchError(error=>{throw error})
+  updateProfileImage(url: string): Observable<boolean> {
+    return this.http.patch<boolean>(`${this.apiUrl}/user/profile/image`, { url }).pipe(
+      map(response => response),
+      catchError(error => { throw error })
     )
   }
 
-  uploadQuotationFiles(files: File[]): Observable<FileAttachmentResponse[]>{
+  updateProfile(data: any): Observable<boolean> {
+    return this.http.patch<boolean>(`${this.apiUrl}/user/profile`, data).pipe(
+      map(response => response),
+      catchError(error => { throw error })
+    )
+  }
+
+  uploadQuotationFiles(files: File[]): Observable<FileAttachmentResponse[]> {
     const formData = new FormData();
     files.forEach(file => {
       formData.append('files', file);
     });
-    formData.append('folder','Visual Forge/Quotation Attachments');
-    return this.http.post<FileAttachmentResponse[]>(`${this.apiUrl}/user/quotation/files-upload`, formData,{ reportProgress: true}).pipe(
-      catchError(error=> { throw error})
+    formData.append('folder', 'Visual Forge/Quotation Attachments');
+    return this.http.post<FileAttachmentResponse[]>(`${this.apiUrl}/user/quotations/upload`, formData, { reportProgress: true }).pipe(
+      catchError(error => { throw error })
     )
   }
 
-  updateProfile(data: any): Observable<boolean>{
-    return this.http.patch<boolean>(`${this.apiUrl}/user/profile/update`, data).pipe(
-      map(response=>response),
-      catchError(error=>{throw error})
-    )
-  }
-
-  resetPassword(data: {currentPassword: string, newPassword: string}):Observable<any>{
+  resetPassword(data: { currentPassword: string, newPassword: string }): Observable<any> {
     return this.http.patch<any>(`${this.apiUrl}/user/reset-password`, data).pipe(
-      map(response=>response),
-      catchError(error=>{throw error})
+      map(response => response),
+      catchError(error => { throw error })
     )
   }
 
@@ -93,32 +94,32 @@ export class UserService {
     );
   }
 
-  rateWork(workId: string, rating: number, feedback: string): Observable<boolean>{
-    console.log('worksId',workId);
-    return this.http.post<boolean>(`${this.apiUrl}/user/quotations/rate`, {workId, rating, feedback}).pipe(
-      map(response=>response),
-      catchError(error=>{throw error})
+  rateWork(workId: string, rating: number, feedback: string): Observable<boolean> {
+    console.log('worksId', workId);
+    return this.http.put<boolean>(`${this.userApiUrl}/quotations/${workId}/rating`, { rating, feedback }).pipe(
+      map(response => response),
+      catchError(error => { throw error })
     )
   }
 
-  rateEditor(editorId: string, rating: number, feedback: string): Observable<boolean>{
-    return this.http.post<boolean>(`${this.apiUrl}/user/editor/rate`, {editorId, rating, feedback}).pipe(
-      map(response=>response),
-      catchError(error=>{throw error})
+  rateEditor(editorId: string, rating: number, feedback: string): Observable<boolean> {
+    return this.http.post<boolean>(`${this.userApiUrl}/editor/rating`, { editorId, rating, feedback }).pipe(
+      map(response => response),
+      catchError(error => { throw error })
     )
   }
 
-  getCurrentEditorRating(editorId: string): Observable<{rating: number, feedback: string}>{
-    return this.http.get<{rating: number, feedback: string}>(`${this.apiUrl}/user/editor/rating`, {params: {editorId}}).pipe(
-      map(response=>response),
-      catchError(error=>{throw error})
+  getCurrentEditorRating(editorId: string): Observable<{ rating: number, feedback: string }> {
+    return this.http.get<{ rating: number, feedback: string }>(`${this.userApiUrl}/editor/rating`, { params: { editorId } }).pipe(
+      map(response => response),
+      catchError(error => { throw error })
     )
   }
 
   updateWorkPublicStatus(workId: string, isPublic: boolean): Observable<boolean> {
     return this.http.patch<boolean>(`${this.apiUrl}/user/quotations/${workId}/public`, { isPublic }).pipe(
-        map(response => response),
-        catchError(error => { throw error })
+      map(response => response),
+      catchError(error => { throw error })
     );
   }
 
@@ -134,15 +135,15 @@ export class UserService {
     );
   }
 
-  getUser(id:string): Observable<User>{
-    return this.http.get<User>(`${this.apiUrl}/user/${id}`).pipe(
-      map(response=>response),
-      catchError(error=>{throw error})
+  getUser(id: string): Observable<User> {
+    return this.http.get<User>(`${this.userApiUrl}/${id}`).pipe(
+      map(response => response),
+      catchError(error => { throw error })
     )
   }
 
-  getEditor(id:string): Observable<any>{
-    return this.http.get<any>(`${this.apiUrl}/user/getEditor/${id}`).pipe(
+  getEditor(id: string): Observable<any> {
+    return this.http.get<any>(`${this.userApiUrl}/editors/${id}`).pipe(
       catchError(error => { throw error })
     );
   }
